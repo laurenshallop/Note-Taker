b18b3e6798616855dcd23a541a7718b40e69a1fb
Lauren Lucio <laurenlucio@Laurens-MacBook-Pro.local>
2022-03-06 22:59:15 -0800

    final

--- server.js
+++ server.js
@@ -1,23 +1,64 @@
-// request to front end for data
-const { data } = require('./data/db.json')
-// setting up express
+// Dependencies
 const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const util = require("util");
 
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
+
+// Server
 const app = express();
+const PORT = process.env.PORT || 3001;
 
-//const PORT = process.env.PORT || 3001;
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 
-//app.use(express.static('public'));
+// Static Middleware
+app.use(express.static('./develop/public'));
 
-// add the route
-app.get('/api/notes', (req, res) => {
-    res.json(notes);
+// add the route / "GET" request
+app.get("/api/notes", function(req, res) {
+    readFileAsync("./develop/db/db.json", "utf8").then(function(data) {
+        notes = [].concat(JSON.parse(data))
+        res.json(notes);
+    })
 });
+  
+// add the route / "POST" request
+app.post("/api/notes", function(req, res) {
+    const note = req.body;
+    readFileAsync("./develop/db/db.json", "utf8").then(function(data) {
+        const notes = [].concat(JSON.parse(data));
+        note.id = notes.length + 1
+        notes.push(note);
+        return notes
+    }).then(function(notes) {
+        writeFileAsync("./develop/db/db.json", JSON.stringify(notes))
+        res.json(note);
+    })
+});
+
+// HTML Route
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "./develop/public/notes.html"));
+});
+
+app.get("/", function(res, req) {
+    res.sendFile(path.join(__dirname, "./develop/public/index.html"));
+});
+
+app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "./develop/public/index.html"));
+});
+
 
 // Server to listen
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
+app.listen(PORT, function() {
+    console.log("API server now on PORT" + PORT);
 });
+
+
 
 //app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`));
